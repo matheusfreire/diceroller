8510589cc939b905ab5d24aeb0a6a1bfad49eab7
Matheus Freire <matheus.freire@sicoob.com.br>
2019-05-14 11:53:18 -0300

    Add when fragment

--- app/src/main/java/com/msf/diceroller/MainActivity.kt
+++ app/src/main/java/com/msf/diceroller/MainActivity.kt
@@ -3,8 +3,10 @@ package com.msf.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,7 +21,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "Dice rolled"
+        val randomInt = Random().nextInt(6) + 1
+        val diceImage : ImageView = findViewById(R.id.dice_image)
+        val drawableResouce = when(randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResouce)
     }
 }
